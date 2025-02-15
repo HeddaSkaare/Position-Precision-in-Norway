6df487f671cf9ed1570aaf72a0b932f8f3e4f8f4
simenmas <simen.masdal@gmail.com>
2025-02-15 11:39:12 +0100

    progress in filterTerrain()

--- backend/FilterWithTerrain.py
+++ backend/FilterWithTerrain.py
@@ -43,16 +43,44 @@ def UTMtilRastercelle(N,E):
 def filterTerrain(df, position):
 
     """
-    Input: Takes a dataframe consisting of Id, time, cartesian position (X,Y,Z), azimuth and zenith.
+    Input: Takes a list of list of dataframes consisting of Id, time, cartesian position (X,Y,Z), azimuth and zenith.
 
     Output: Dataframe of satellites not blocked by the terrain at the checked coordinate, same dimension as the input.
     """
 
-    filename, index = UTMtilRastercelle(position[0],position[1])
+    elevation_mask = 10
 
-    satellite = df[0][0]
+    filename, cell_index = UTMtilRastercelle(position[0],position[1])
 
-    print(satellite)
+    satellites = df
+    for epoch in satellites:
+        for gnss_type in epoch:
+            for index, gnss in gnss_type.iterrows():
 
+                baseheight = data[cell_index[0],cell_index[1]]
+                print(gnss['azimuth'])
+                angle = (gnss['azimuth']-90)%360
+                obsangle = 0
+                while obsangle < gnss['zenith']:
+
+                    cell_index[0]+=1
+                    cell_index[1]+=np.sin(angle)
+                    height = data[int(cell_index[0]),int(cell_index[1])]
+                    distance = np.sqrt(int(cell_index[0]-position[0])**2+int(cell_index[1]-position[1])**2)
+                    obsangle = (height-baseheight)/distance
+
+                    if distance > 750:
+                        #Satellite is visible
+                        #add gnss to new dataframe
+                        obsangle = 90
+
+
+                break
+            break
+        break
+
+
+    #print(satellite)
+#print('hei')
 test = runData(['GPS', 'Galileo', 'GLONASS', 'BeiDou'], '10', '2025-02-12T12:00:00.000', 4)
 filterTerrain(test[1], position)
