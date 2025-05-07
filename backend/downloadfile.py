4208a8c2efad21be178812c384726e89930787a0
HeddaSkaare <heddsk@stud.ntnu.no>
2025-05-07 13:35:00 +0200

    fix maby

--- backend/downloadfile.py
+++ backend/downloadfile.py
@@ -7,7 +7,9 @@ folder = 'unzipped/'
 def lastned(day, year):
     print(day)
     filename = f'BRD400DLR_S_{year}{day}0000_01D_MN.rnx.gz'
-
+    
+    # ✅ Sørg for at mappen finnes:
+    os.makedirs(folder, exist_ok=True)
     url = f'https://cddis.nasa.gov/archive/gnss/data/daily/{year}/brdc/{filename}'
 
     if not os.path.isfile(folder+filename[:-3]):
